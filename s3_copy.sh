3c677101415b2bb3e6ddb06b0ca97a9e1e3c0011
Akhilesh <akhilesh.k@beehyv.com>
2023-09-11 19:53:25 +0530

    Fix included

--- s3_copy.sh
+++ s3_copy.sh
@@ -98,6 +98,6 @@ declare -a coreplugins=("org.ekstep.activitybrowser-1.3"
 
 for i in "${coreplugins[@]}"
 do
-   aws s3 --region ap-south-1 rm s3://ekstep-public-$1/content-plugins/$i/ --recursive
-   aws s3 --region ap-south-1 cp ansible/content-plugins/$i s3://ekstep-public-$1/content-plugins/$i --recursive --acl public-read
+   aws s3 --region ap-south-1 rm s3://ekstep-public-$1/content-plugins/$i/ 
+   aws s3 --region ap-south-1 cp ansible/content-plugins/$i s3://ekstep-public-$1/content-plugins/$i --acl public-read
 done
